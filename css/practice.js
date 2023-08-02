f0cf3e37d325689f2b450f0396e1b91ad32be18d
Kim-Collins <collinskimotho16@gmail.com>
2023-08-02 14:04:31 +0300

    add

---
+++ css/practice.js
@@ -0,0 +1,14 @@
+let bootcamp = "App Academy";
+console.log(bootcamp);
+
+let birthyear = 2012;
+console.log(birthyear);
+
+let num;
+console.log(num * 2);
+
+let x = 7;
+x++;
+x - 2;
+x *=3;
+console.log(x);
